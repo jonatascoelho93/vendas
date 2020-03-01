8e3ed7e4b0ffec3818b30630e68a2e69ef3a393c
Douglas Oliveira <douglas.oiveira@credsystem.com.br>
2020-03-01 12:06:25 -0300

    adicinado dependencia de teste

---
+++ src/test/java/br/com/estudo/Teste.java
@@ -0,0 +1,17 @@
+package br.com.estudo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class Teste {
+	
+	@Test
+	public void teste() {
+		
+	}
+
+}
